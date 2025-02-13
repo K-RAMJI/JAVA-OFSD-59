66665a2e8e7cd85eb3e2fb14c9bb1e6c2bf9046e
K RAMJI <k.ramjics@gmail.com>
2025-02-13 11:58:03 +0530

    Complete Sec_01_Introduction

---
+++ src/Sec_01_Introduction/First_Program.java
@@ -0,0 +1,10 @@
+package Sec_01_Introduction;
+import java.lang.*; //Automatically gets imported if not mentioned
+
+public class First_Program {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Universe !!!");
+    }
+}
